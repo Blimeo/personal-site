ff2818494aa79c9194066390fd1ae3590c996598
Matthew Ye <my1@berkeley.edu>
2023-04-12 12:17:15 +0900

    yuh

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 
+import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/functions";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
-	// Enable Svelte to support Svelte components.
-	integrations: [svelte()],
+  // Enable Svelte to support Svelte components.
+  integrations: [svelte(), tailwind(), sitemap(), robotsTxt()],
+  output: "server",
+  adapter: netlify()
 });
